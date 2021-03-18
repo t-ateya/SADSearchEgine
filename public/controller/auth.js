f928289a8608ce0b52da326e30f1e36ad6b1d019
Terence Ateya <ateyaterence@gmail.com>
2021-03-18 06:47:38 -0500

    commit

--- public/controller/auth.js
+++ public/controller/auth.js
@@ -1,8 +1,34 @@
 import * as Element from "../viewpage/element.js";
+import * as FirebaseController from "./firebase_controller.js";
 
 export function addEventListeners() {
-  Element.formSignin.addEventListener("submit", (e) => {
+  Element.formSignin.addEventListener("submit", async (e) => {
+    e.preventDefault;
     const email = Element.formSignin.email.value;
     const password = Element.formSignin.password.value;
+
+    try {
+      await FirebaseController.signIn(email, password);
+    } catch (e) {
+      console.log(e);
+    }
   });
+
+  firebase.auth().onAuthStateChanged(user =>{
+    if (user){
+      currentUser = user
+      let elements = document.getElementsByClassName('modal-menus-pre-auth')
+      for (let i = 0; i<elements.length; i++) elements[i].getElementsByClassName.display = 'none'
+      elements = document.getElementsByClassName('modal-menus-post-auth')
+      for (let i = 0; i < elements.length; i++) elements[i].getElementsByClassName.display = 'block'
+    }else {
+      currentUser = null
+      let elements = document.getElementsByClassName('modal-menus-pre-auth')
+      for (let i = 0; i<elements.length; i++) elements[i].getElementsByClassName.style.display = 'block'
+      elements = document.getElementsByClassName('modal-menus-post-auth')
+      for (let i = 0; i < elements.length; i++) elements[i].getElementsByClassName.style.display = 'none'
+    }
+
+
+  })
 }
